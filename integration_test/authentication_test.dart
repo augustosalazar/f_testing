17ce1ef06cec73c280f17cc6a00bb3e63ec81e66
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2023-09-06 16:02:14 -0500

    Fix nav error and included new test case

--- integration_test/authentication_test.dart
+++ integration_test/authentication_test.dart
@@ -11,11 +11,6 @@ void main() {
 
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets("Login sin creación de usuario", (WidgetTester tester) async {
-    Widget w = await createHomeScreen();
-    await tester.pumpWidget(w);
-  });
-
   testWidgets("Login -> signup -> creación usuario -> login no exitoso",
       (WidgetTester tester) async {
     Widget w = await createHomeScreen();
@@ -58,6 +53,44 @@ void main() {
       (WidgetTester tester) async {
     Widget w = await createHomeScreen();
     await tester.pumpWidget(w);
+
+    expect(find.byKey(const Key('LoginScreen')), findsOneWidget);
+
+    await tester.tap(find.byKey(const Key('ButtonLoginCreateAccount')));
+
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(const Key('SignUpPage')), findsOneWidget);
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldSignUpEmail')), 'a@a.com');
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldSignUpPassword')), '123456');
+
+    await tester.tap(find.byKey(const Key('ButtonSignUpSubmit')));
+
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(const Key('LoginScreen')), findsOneWidget);
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldLoginEmail')), 'a@a.com');
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldLoginPassword')), '123456');
+
+    await tester.tap(find.byKey(const Key('ButtonLoginSubmit')));
+
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(const Key('LoginScreen')), findsNothing);
+
+    await tester.tap(find.byKey(const Key('ButtonHomeLogOff')));
+
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(const Key('LoginScreen')), findsOneWidget);
   });
 
   testWidgets(
